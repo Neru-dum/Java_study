af755ec38a62a0fe24a5c71c0c2ee1326c46b08b
Neru-dum <tanten180@gmail.com>
2020-04-07 00:03:25 +0900

    基本データ型

--- MyApp.java
+++ MyApp.java
@@ -1,11 +1,20 @@
 
 public class MyApp {
   public static void main(String[] args){
-    // 変数
-    // 型　変数名
-    // String msg;
-    // msg = "Hellow World Again";
-    String msg = "Hellow World Again";
+    // 文字 単一文字
+    char a = 'a';    
+    //整数　byte short int long
+    int x = 10;
+    long y = 5555555555L;
+    //浮動小数点 float double
+    double d = 23523523.322;
+    float f = 32.33F;
+    //　論理値
+    boolean flag = true; //flase
+    //文字列 特殊文字
+    // \n
+    // \t
+    String msg = "Hello Wo\nuld Again\t!";
     System.out.println(msg);
   }
 }
