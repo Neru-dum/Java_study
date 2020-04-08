b7e8263e8205252b59d816526b31997affd64ba9
Neru-dum <tanten180@gmail.com>
2020-04-09 08:26:00 +0900

    データの演算

--- MyApp.java
+++ MyApp.java
@@ -1,20 +1,27 @@
 
 public class MyApp {
   public static void main(String[] args){
-    // 文字 単一文字
-    char a = 'a';    
-    //整数　byte short int long
-    int x = 10;
-    long y = 5555555555L;
-    //浮動小数点 float double
-    double d = 23523523.322;
-    float f = 32.33F;
-    //　論理値
-    boolean flag = true; //flase
-    //文字列 特殊文字
-    // \n
-    // \t
-    String msg = "Hello Wo\nuld Again\t!";
-    System.out.println(msg);
+    // 演算
+    // + - * / %
+    // ++ --
+    // int i;
+    // i = 10 / 3;
+    // System.out.println(i); //3
+    // i = 10 % 3;
+    // System.out.println(i); //1
+    // int x = 5;
+    // x++;
+    // System.out.println(x); //6
+    // x--;
+    // System.out.println(x); //5
+
+    // int x = 5;
+    // // x = x + 12;
+    // x += 12;
+    // System.out.println(x); //17
+
+    String s;
+    s = "hello " + "world";
+    System.out.println(s);
   }
 }
