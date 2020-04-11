514ebde44c80f0f67fb1c8a19c1e629fb9a19ef5
Neru-dum <tanten180@gmail.com>
2020-04-11 23:49:13 +0900

    データ型の変換

--- MyApp.java
+++ MyApp.java
@@ -1,27 +1,13 @@
 
 public class MyApp {
-  public static void main(String[] args){
-    // 演算
-    // + - * / %
-    // ++ --
-    // int i;
-    // i = 10 / 3;
-    // System.out.println(i); //3
-    // i = 10 % 3;
-    // System.out.println(i); //1
-    // int x = 5;
-    // x++;
-    // System.out.println(x); //6
-    // x--;
-    // System.out.println(x); //5
+  public static void main(String[] args){    
+    //キャスト
+    // double d = 5535353.232;
+    // int i = (int)d;
+    // System.out.println(i);
 
-    // int x = 5;
-    // // x = x + 12;
-    // x += 12;
-    // System.out.println(x); //17
-
-    String s;
-    s = "hello " + "world";
-    System.out.println(s);
+    int i = 10;
+    double d = (double)i /4;
+    System.out.println(d);
   }
 }
