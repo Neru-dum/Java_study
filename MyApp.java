eed678e20a8b06468ae42a81eaca94ebad108d27
Neru-dum <tanten180@gmail.com>
2020-04-25 23:28:20 +0900

    配列の要素の操作

--- MyApp.java
+++ MyApp.java
@@ -2,20 +2,13 @@
 public class MyApp {
   public static void main(String[] args){    
     // 配列
-    //sales1,sales2....
-    //sales
-    // int[] sales;
-    // sales = new int[3];
-    // sales[0] = 100;
-    // sales[1] = 200;
-    // sales[2] = 300;
-
-    // int [] sales;
-    // sales = new int[3] {100, 200, 300};
-
-    int[] sales = {100, 200, 300};
-      System.out.println(sales[1]);    
-    sales[1] = 1000;
-    System.out.println(sales[1]);    
+    // sales.length
+    int[] sales = {700,400,500};
+    // for (int i = 0; i< sales.length; i++){
+    //   System.out.println(sales[i]);      
+    // }
+    for (int sale : sales) {
+      System.out.println(sale);    
+    }
   }
 }
