0ac9af19b1241ed28ca25caa4035e4ecfdf444f6
Neru-dum <tanten180@gmail.com>
2020-04-28 22:56:18 +0900

    メソッドのオーバーロード

--- MyApp.java
+++ MyApp.java
@@ -1,18 +1,20 @@
 
 public class MyApp {
-  
-  // method
-  // public static void sayHi()
-  // public static void sayHi(String name)
-  public static String sayHi(String name){
-    // System.out.println("Hi!");
-    // System.out.println("Hi!" + name);
-    return "Hi! " + name;
+
+  public static void sayHi(String name){
+    // int x = 10;
+    System.out.println("Hi!" + name);
   }
-  public static void main(String[] args){    
-    // sayHi("Tom");  
-    // sayHi("Tom");  
-    String msg = sayHi("Steve");
-    System.out.println(msg);
+
+  // overload
+  public static void sayHi(){
+    // int x = 10;
+    System.out.println("Hi! Nobady!");
+  }
+  public static void main(String[] args){   
+    sayHi(); 
+    sayHi("Steve"); //引数
+    // System.out.println(name);
+    // System.out.println(x);
   }
 }
