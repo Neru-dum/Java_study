92ca9ce1106f4eddc5d93b19d655f90ce2ae3030
Neru-dum <tanten180@gmail.com>
2020-05-02 11:51:50 +0900

    クラスの継承

--- MyApp.java
+++ MyApp.java
@@ -1,25 +1,40 @@
 
 class User {
   String name; 
-   // constructor
+
   User(String name){
     this.name = name;
   }
-  // this
-  User(){
-    // this.name = "Me!";
-    this("Tom!");
-  }
+
   void sayHi(){
     System.out.println("hi!" + this.name);
   }
 }
+  class AdminUser extends User{
+    AdminUser(String name){
+      super(name);
+    }
+    void sayHello(){
+      System.out.println("hello!" + this.name);
+    }
+
+    // override
+    @Override
+    void sayHi(){
+      System.out.println("[admin] hi!" + this.name);
+    }
+  }  
+
+
 public class MyApp {
   public static void main(String[] args){   
-    User tom; //参照型
-    // tom = new User("Tom");
-    tom = new User();
+    User tom = new User("tom");
     System.out.println(tom.name);
     tom.sayHi();
+
+    AdminUser bob = new AdminUser("bob");
+    System.out.println(bob.name);
+    bob.sayHi();
+    bob.sayHello();
   }
 }
